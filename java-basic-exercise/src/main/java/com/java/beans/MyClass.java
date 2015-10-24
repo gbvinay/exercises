1ca81b01a9fbe0f67cdb20b3bf09619101b14d57
Vinay G  B <Vinay.X.G.B.-ND@espn.com>
2015-10-24 19:37:59 +0530

    First Commit

---
+++ java-basic-exercise/src/main/java/com/java/beans/MyClass.java
@@ -0,0 +1,17 @@
+package com.java.beans;
+
+/**
+ * Created by GBV on 10/24/2015.
+ */
+public class MyClass {
+
+    private String firstVariable;
+
+    public String getFirstVariable() {
+        return firstVariable;
+    }
+
+    public void setFirstVariable(String firstVariable) {
+        this.firstVariable = firstVariable;
+    }
+}
