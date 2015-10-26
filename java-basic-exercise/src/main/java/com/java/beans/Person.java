1dec47c06e3d9efb95c3b17ef919f3284d7fccc2
Vinay.X.G.B.-ND@espn.com <Vinay.X.G.B.-ND@espn.com>
2015-10-26 22:42:07 +0530

    Builder Patterns For JavaBeans

---
+++ java-basic-exercise/src/main/java/com/java/beans/Person.java
@@ -0,0 +1,93 @@
+    package com.java.beans;
+
+    import java.util.Date;
+
+    /**
+     * Created by gbv on 10/26/2015.
+     */
+    public class Person {
+
+        private String firstName;
+        private String lastName;
+        private Long personId;
+        private Date startDate;
+        private Date endDate;
+
+
+        public static void main(String[] args) {
+            PersonObject.printPersonObject();
+        }
+
+
+        private static class PersonBuilder{
+           //Required Parameters
+            private String firstName;
+            private String lastName;
+            private Long personId;
+
+            //Optional Parameter
+            private Date startDate;
+            private Date endDate;
+
+
+            public PersonBuilder(String firstName,String lastName, Long personId){
+                this.firstName = firstName;
+                this.lastName = lastName;
+                this.personId = personId;
+            }
+
+            public PersonBuilder startDate(Date startDate){
+                this.startDate = startDate;
+                return this;
+            }
+
+            public PersonBuilder endDate(Date endDate){
+                this.endDate = endDate;
+                return this;
+            }
+
+            public Person build(){
+                return new Person(this);
+            }
+
+        }
+
+        private Person(PersonBuilder personBuilder){
+            firstName = personBuilder.firstName;
+            lastName = personBuilder.lastName;
+            personId = personBuilder.personId;
+            startDate = personBuilder.startDate;
+            endDate = personBuilder.endDate;
+        }
+
+        public String getFirstName(){
+            return firstName;
+        }
+
+        public String getLastName(){
+            return lastName;
+        }
+
+        public Long getPersonId(){
+            return personId;
+        }
+
+        public Date getStartDate(){
+            return startDate;
+        }
+
+        public Date getEndDate() {
+            return endDate;
+        }
+
+        private static class PersonObject{
+
+                public static void printPersonObject() {
+                    Date stDate = new Date();
+                    Person personObject = new Person.PersonBuilder("Vinay","G B",1234L).startDate(new Date()).build();
+                    System.out.println("personObject : "+personObject.getFirstName());
+                }
+        }
+
+    }
+
